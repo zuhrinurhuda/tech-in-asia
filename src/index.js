518c896f4a07523fb5e8f839001a955f06bd22d6
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2018-02-26 09:31:34 +0700

    setup dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import './assets/styles/index.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
